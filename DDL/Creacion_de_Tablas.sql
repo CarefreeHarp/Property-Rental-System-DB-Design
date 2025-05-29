9922b865a9cc724e019fc5d33810cf3e278df91f
CarefreeHarp <daniramirezva@gmail.com>
2025-05-28 22:46:40 -0500

    Prueba

--- DDL/Creacion_de_Tablas.sql
+++ DDL/Creacion_de_Tablas.sql
@@ -171,5 +171,4 @@ CREATE TABLE Control_Changes (
     column_name   VARCHAR2(50),         -- Nombre del atributo modificado
     old_value     VARCHAR2(500),        
     new_value     VARCHAR2(500)         
-); 
---Prueba
+);
