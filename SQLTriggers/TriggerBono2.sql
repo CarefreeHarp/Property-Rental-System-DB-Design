6fe1b3d8f23f3651a5fa7970763bb84772e27b76
guille-011 <apontecardenas54321@gmail.com>
2025-05-29 22:48:44 -0500

    Trigger notificaciones contrato

---
+++ SQLTriggers/TriggerBono2.sql
@@ -0,0 +1,22 @@
+create or replace trigger tgr_notification_contract after
+   insert on contract
+   for each row
+declare
+   v_username varchar2(30);
+begin
+   select username
+     into v_username
+     from client
+    where client_id = :new.client_id;
+   insert into notification (
+      hour_date,
+      message,
+      typology,
+      username
+   ) values ( sysdate,
+              'Your contract has been created',
+              'FIRMED CONTRACT',
+              v_username );
+   commit;
+end;
+/
