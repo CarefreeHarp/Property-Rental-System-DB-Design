f4b01d415f20d5e707dacf14d1409541885f3250
intrepido1902 <srayo1902@gmail.com>
2025-05-29 20:36:03 -0500

    Update Trigger3.sql

--- SQLTriggers/Trigger3.sql
+++ SQLTriggers/Trigger3.sql
@@ -23,4 +23,6 @@ BEGIN
     END IF;
 END;
 
+
+--se tuvo que agregar el atributo is_deleted a la tabla property
 ALTER TABLE Property ADD is_deleted CHAR(1) DEFAULT 'N' CHECK (is_deleted IN ('Y', 'N'));
