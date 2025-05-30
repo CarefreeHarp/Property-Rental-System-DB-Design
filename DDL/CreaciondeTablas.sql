c8467f00cea4d7175c13cba9938f33f3fa5f3240
guille-011 <apontecardenas54321@gmail.com>
2025-05-29 21:27:23 -0500

    tabla de notificaciones

--- DDL/CreaciondeTablas.sql
+++ DDL/CreaciondeTablas.sql
@@ -1,165 +1,233 @@
-CREATE TABLE Account_Information (
-    Username VARCHAR2(20) PRIMARY KEY,
-    max_rent NUMBER(7,2) DEFAULT 0 NOT NULL CHECK (max_rent >= 0),
-    type VARCHAR2(6) DEFAULT '-' NOT NULL CHECK (type IN ('CLIENT','OWNER')),
-    creation_date DATE DEFAULT TRUNC(SYSDATE) NOT NULL,
-    name VARCHAR2(30) DEFAULT '-' NOT NULL,
-    lastname VARCHAR2(30) DEFAULT '-' NOT NULL,
-    in_deleting CHAR(1) DEFAULT 'N' NOT NULL CHECK (in_deleting IN ('Y', 'N'))
+create table account_information (
+   username      varchar2(20) primary key,
+   max_rent      number(7,2) default 0 not null check ( max_rent >= 0 ),
+   type          varchar2(6) default '-' not null check ( type in ( 'CLIENT',
+                                                           'OWNER' ) ),
+   creation_date date default trunc(sysdate) not null,
+   name          varchar2(30) default '-' not null,
+   lastname      varchar2(30) default '-' not null,
+   in_deleting   char(1) default 'N' not null check ( in_deleting in ( 'Y',
+                                                                     'N' ) )
 );
 
-CREATE TABLE Account_Credential (
-    Username VARCHAR2(20) PRIMARY KEY,
-    email VARCHAR2(30) DEFAULT '-' NOT NULL,
-    password VARCHAR2(30) DEFAULT '-' NOT NULL,
-    CONSTRAINT unique_email UNIQUE (email)
+create table account_credential (
+   username varchar2(20) primary key,
+   email    varchar2(30) default '-' not null,
+   password varchar2(30) default '-' not null,
+   constraint unique_email unique ( email )
 );
 
-CREATE TABLE Owner (
-    owner_id VARCHAR2(10) PRIMARY KEY,
-    username VARCHAR2(20) DEFAULT '-' NOT NULL,
-    id_type VARCHAR2(2) DEFAULT 'CC' NOT NULL CHECK (id_type IN ('CC', 'TI', 'CE')),
-    FOREIGN KEY (username) REFERENCES Account_Information(Username) ON DELETE CASCADE
+create table owner (
+   owner_id varchar2(10) primary key,
+   username varchar2(20) default '-' not null,
+   id_type  varchar2(2) default 'CC' not null check ( id_type in ( 'CC',
+                                                                  'TI',
+                                                                  'CE' ) ),
+   foreign key ( username )
+      references account_information ( username )
+         on delete cascade
 );
 
-CREATE TABLE Client (
-    client_id VARCHAR2(10) PRIMARY KEY,
-    username VARCHAR2(20) DEFAULT '-' NOT NULL,
-    id_type VARCHAR2(2) DEFAULT 'CC' NOT NULL CHECK (id_type IN ('CC', 'TI', 'CE')),
-    FOREIGN KEY (username) REFERENCES Account_Information(Username) ON DELETE CASCADE
+create table client (
+   client_id varchar2(10) primary key,
+   username  varchar2(20) default '-' not null,
+   id_type   varchar2(2) default 'CC' not null check ( id_type in ( 'CC',
+                                                                  'TI',
+                                                                  'CE' ) ),
+   foreign key ( username )
+      references account_information ( username )
+         on delete cascade
 );
 
-CREATE TABLE Property (
-    property_id VARCHAR2(10) PRIMARY KEY,
-    owner_id VARCHAR2(10) DEFAULT '-' NOT NULL,
-    num_rooms NUMBER(2) DEFAULT 1 NOT NULL CHECK (num_rooms > 1 and num_rooms < 99 ),
-    location VARCHAR2(100) DEFAULT '-' NOT NULL,
-    description VARCHAR2(500) DEFAULT '-' NOT NULL,
-    available CHAR(1) DEFAULT 'N' NOT NULL CHECK (available IN ('Y', 'N')),
-    type VARCHAR2(11) DEFAULT '-' NOT NULL CHECK (type IN ('Casa', 'Apartamento')),
-    address VARCHAR2(30) DEFAULT '-' NOT NULL,
-    base_rent NUMBER(7,2) DEFAULT 0 NOT NULL CHECK (base_rent >= 0),
-    availability_date DATE DEFAULT TRUNC(SYSDATE) NOT NULL,
-    discount NUMBER(10,5) DEFAULT 0 NOT NULL CHECK (discount >= 0),
-    discount_type VARCHAR2(10) DEFAULT 'PERCENTAGE' NOT NULL CHECK (discount_type IN ('PERCENTAGE', 'VALUE')), 
-    FOREIGN KEY (owner_id) REFERENCES Owner(owner_id) ON DELETE CASCADE,
-    CONSTRAINT unique_address UNIQUE (address)
+create table property (
+   property_id       varchar2(10) primary key,
+   owner_id          varchar2(10) default '-' not null,
+   num_rooms         number(2) default 1 not null check ( num_rooms > 1
+      and num_rooms < 99 ),
+   location          varchar2(100) default '-' not null,
+   description       varchar2(500) default '-' not null,
+   available         char(1) default 'N' not null check ( available in ( 'Y',
+                                                                 'N' ) ),
+   type              varchar2(11) default '-' not null check ( type in ( 'Casa',
+                                                            'Apartamento' ) ),
+   address           varchar2(30) default '-' not null,
+   base_rent         number(7,2) default 0 not null check ( base_rent >= 0 ),
+   availability_date date default trunc(sysdate) not null,
+   discount          number(10,5) default 0 not null check ( discount >= 0 ),
+   discount_type     varchar2(10) default 'PERCENTAGE' not null check ( discount_type in ( 'PERCENTAGE',
+                                                                                       'VALUE' ) ),
+   foreign key ( owner_id )
+      references owner ( owner_id )
+         on delete cascade,
+   constraint unique_address unique ( address )
 );
 
-CREATE TABLE Tenant (
-    client_id VARCHAR2(10) NOT NULL,
-    property_id VARCHAR2(10) NOT NULL,
-    start_date DATE DEFAULT TRUNC(SYSDATE) NOT NULL,
-    end_date DATE DEFAULT TRUNC(SYSDATE) NOT NULL,
-    PRIMARY KEY (client_id, start_date),
-    FOREIGN KEY (client_id) REFERENCES Client(client_id) ON DELETE CASCADE,
-    FOREIGN KEY (property_id) REFERENCES Property(property_id) ON DELETE CASCADE
+create table tenant (
+   client_id   varchar2(10) not null,
+   property_id varchar2(10) not null,
+   start_date  date default trunc(sysdate) not null,
+   end_date    date default trunc(sysdate) not null,
+   primary key ( client_id,
+                 start_date ),
+   foreign key ( client_id )
+      references client ( client_id )
+         on delete cascade,
+   foreign key ( property_id )
+      references property ( property_id )
+         on delete cascade
 );
 
-CREATE TABLE Visit (
-    property_id VARCHAR2(10) NOT NULL,
-    client_id VARCHAR2(10) NOT NULL,
-    start_time DATE DEFAULT TRUNC(SYSDATE) NOT NULL,
-    duration NUMBER(6,2) DEFAULT 0 NOT NULL CHECK (duration >= 0),
-    end_time DATE DEFAULT TRUNC(SYSDATE) NOT NULL,
-    PRIMARY KEY (property_id, client_id, start_time),
-    FOREIGN KEY (property_id) REFERENCES Property(property_id) ON DELETE CASCADE,
-    FOREIGN KEY (client_id) REFERENCES Client(client_id) ON DELETE CASCADE
+create table visit (
+   property_id varchar2(10) not null,
+   client_id   varchar2(10) not null,
+   start_time  date default trunc(sysdate) not null,
+   duration    number(6,2) default 0 not null check ( duration >= 0 ),
+   end_time    date default trunc(sysdate) not null,
+   primary key ( property_id,
+                 client_id,
+                 start_time ),
+   foreign key ( property_id )
+      references property ( property_id )
+         on delete cascade,
+   foreign key ( client_id )
+      references client ( client_id )
+         on delete cascade
 );
 
-CREATE TABLE Service (
-    service_name VARCHAR2(50) PRIMARY KEY,
-    person_value NUMBER(7,2) DEFAULT 0 NOT NULL CHECK (person_value >= 0)
+create table service (
+   service_name varchar2(50) primary key,
+   person_value number(7,2) default 0 not null check ( person_value >= 0 )
 );
 
-CREATE TABLE Payment (
-    payment_id VARCHAR2(10) PRIMARY KEY,
-    client_id VARCHAR2(10) DEFAULT '-' NOT NULL,
-    value NUMBER(8,2) DEFAULT 0 NOT NULL CHECK (value > 0),
-    email VARCHAR2(30) DEFAULT '-' NOT NULL,
-    cash_used CHAR(1) DEFAULT 'N' NOT NULL CHECK (cash_used IN ('Y', 'N')),
-    cash_value NUMBER(8,2) DEFAULT NULL CHECK (cash_value >= 0),
-    payment_date DATE DEFAULT TRUNC(SYSDATE) NOT NULL,
-    contract_id VARCHAR2(10) DEFAULT '-' NOT NULL,
-    FOREIGN KEY (client_id) REFERENCES Client(client_id) ON DELETE CASCADE
+create table payment (
+   payment_id   varchar2(10) primary key,
+   client_id    varchar2(10) default '-' not null,
+   value        number(8,2) default 0 not null check ( value > 0 ),
+   email        varchar2(30) default '-' not null,
+   cash_used    char(1) default 'N' not null check ( cash_used in ( 'Y',
+                                                                 'N' ) ),
+   cash_value   number(8,2) default null check ( cash_value >= 0 ),
+   payment_date date default trunc(sysdate) not null,
+   contract_id  varchar2(10) default '-' not null,
+   foreign key ( client_id )
+      references client ( client_id )
+         on delete cascade
 );
 
 
-CREATE TABLE Card (
-    payment_id VARCHAR2(10) PRIMARY KEY,
-    card_number VARCHAR2(19) DEFAULT '0-0-0-0' NOT NULL,
-    card_owner VARCHAR2(60) DEFAULT '-' NOT NULL,
-    type VARCHAR2(10) DEFAULT '-' NOT NULL CHECK (type IN ('Visa', 'MasterCard')),
-    expiration_year VARCHAR2(2) DEFAULT '0' NOT NULL,
-    expiration_month VARCHAR2(2) DEFAULT '0' NOT NULL,
-    FOREIGN KEY (payment_id) REFERENCES Payment(payment_id) ON DELETE CASCADE
+create table card (
+   payment_id       varchar2(10) primary key,
+   card_number      varchar2(19) default '0-0-0-0' not null,
+   card_owner       varchar2(60) default '-' not null,
+   type             varchar2(10) default '-' not null check ( type in ( 'Visa',
+                                                            'MasterCard' ) ),
+   expiration_year  varchar2(2) default '0' not null,
+   expiration_month varchar2(2) default '0' not null,
+   foreign key ( payment_id )
+      references payment ( payment_id )
+         on delete cascade
 );
 
-CREATE TABLE Coupon (
-    coupon_number VARCHAR2(10) PRIMARY KEY,
-    payment_id VARCHAR2(10) DEFAULT '-' NOT NULL,
-    value NUMBER(8,2) DEFAULT 0 NOT NULL CHECK (value > 0),
-    is_used CHAR(1) DEFAULT 'N' NOT NULL CHECK (is_used IN ('Y', 'N')),
-    FOREIGN KEY (payment_id) REFERENCES Payment(payment_id)
+create table coupon (
+   coupon_number varchar2(10) primary key,
+   payment_id    varchar2(10) default '-' not null,
+   value         number(8,2) default 0 not null check ( value > 0 ),
+   is_used       char(1) default 'N' not null check ( is_used in ( 'Y',
+                                                             'N' ) ),
+   foreign key ( payment_id )
+      references payment ( payment_id )
 );
 
-CREATE TABLE Waiting_List_Position (
-    clients_id VARCHAR2(10) PRIMARY KEY,
-    property_id VARCHAR2(10) NOT NULL,
-    property_order NUMBER(4) DEFAULT 1 NOT NULL CHECK (property_order >= 1),
-    FOREIGN KEY (property_id) REFERENCES Property(property_id)
+create table waiting_list_position (
+   clients_id     varchar2(10) primary key,
+   property_id    varchar2(10) not null,
+   property_order number(4) default 1 not null check ( property_order >= 1 ),
+   foreign key ( property_id )
+      references property ( property_id )
 );
 
-CREATE TABLE Rent_solicitude (
-    client_id VARCHAR2(10) PRIMARY KEY,
-    property_id VARCHAR2(10) NOT NULL,
-    start_date DATE DEFAULT TRUNC(SYSDATE) NOT NULL,
-    end_date DATE DEFAULT TRUNC(SYSDATE) NOT NULL,
-    preliminary_cost NUMBER(8,2) DEFAULT 0 NOT NULL CHECK (preliminary_cost >= 0),
-    FOREIGN KEY (property_id) REFERENCES Property(property_id) ON DELETE CASCADE,
-    FOREIGN KEY (client_id) REFERENCES Client(client_id) ON DELETE CASCADE
+create table rent_solicitude (
+   client_id        varchar2(10) primary key,
+   property_id      varchar2(10) not null,
+   start_date       date default trunc(sysdate) not null,
+   end_date         date default trunc(sysdate) not null,
+   preliminary_cost number(8,2) default 0 not null check ( preliminary_cost >= 0 ),
+   foreign key ( property_id )
+      references property ( property_id )
+         on delete cascade,
+   foreign key ( client_id )
+      references client ( client_id )
+         on delete cascade
 );
 
-CREATE TABLE Contract(
-    contract_id NUMBER GENERATED ALWAYS AS IDENTITY,
-    client_id VARCHAR2(10) NOT NULL,
-    property_id VARCHAR2(10) NOT NULL,
-    TPV_per_Month NUMBER(8,2) DEFAULT 0 NOT NULL CHECK (TPV_per_Month >= 0),
-    start_date DATE DEFAULT TRUNC(SYSDATE) NOT NULL,
-    end_date DATE DEFAULT TRUNC(SYSDATE) NOT NULL,
-    PRIMARY KEY (contract_id, client_id),
-    CONSTRAINT unique_propertyid unique (property_id),
-    FOREIGN KEY (property_id) REFERENCES Property(property_id) ON DELETE CASCADE,
-    FOREIGN KEY (client_id) REFERENCES Client(client_id) ON DELETE CASCADE
+create table contract (
+   contract_id   number
+      generated always as identity,
+   client_id     varchar2(10) not null,
+   property_id   varchar2(10) not null,
+   tpv_per_month number(8,2) default 0 not null check ( tpv_per_month >= 0 ),
+   start_date    date default trunc(sysdate) not null,
+   end_date      date default trunc(sysdate) not null,
+   primary key ( contract_id,
+                 client_id ),
+   constraint unique_propertyid unique ( property_id ),
+   foreign key ( property_id )
+      references property ( property_id )
+         on delete cascade,
+   foreign key ( client_id )
+      references client ( client_id )
+         on delete cascade
 );
 
-CREATE TABLE Additional_Service (
-    client_id VARCHAR2(10) NOT NULL,
-    service_name VARCHAR2(50) NOT NULL,
-    num_people NUMBER(2) DEFAULT 1 NOT NULL CHECK (num_people > 1 and num_people < 99),
-    value NUMBER(7,2) DEFAULT 0 NOT NULL CHECK (value >= 0),
-    PRIMARY KEY (client_id, service_name),
-    FOREIGN KEY (client_id) REFERENCES Client(client_id) ON DELETE CASCADE,
-    FOREIGN KEY (service_name) REFERENCES Service(service_name) ON DELETE CASCADE
+create table additional_service (
+   client_id    varchar2(10) not null,
+   service_name varchar2(50) not null,
+   num_people   number(2) default 1 not null check ( num_people > 1
+      and num_people < 99 ),
+   value        number(7,2) default 0 not null check ( value >= 0 ),
+   primary key ( client_id,
+                 service_name ),
+   foreign key ( client_id )
+      references client ( client_id )
+         on delete cascade,
+   foreign key ( service_name )
+      references service ( service_name )
+         on delete cascade
 );
 
-CREATE TABLE Additional_Rates ( -- Tabla que incluye la informacion de tarifas adicionales como impuestos o cosas como la administración de las casas
-    rate_name VARCHAR2(50) PRIMARY KEY,
-    rate_value NUMBER(7,2) NOT NULL CHECK (rate_value >= 0),
-    unit VARCHAR2(10) DEFAULT '%' NOT NULL CHECK (unit IN ('%')),
-    rate_type VARCHAR2(20) DEFAULT '-' NOT NULL CHECK (rate_type IN ('TAX', 'COMMISSION')),
-    description VARCHAR2(100) DEFAULT '-' NOT NULL
-); 
-
-
-CREATE TABLE Control_Changes (
-    change_id     NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    table_name    VARCHAR2(50),         -- Nombre de la tabla modificada (ej. 'Client', 'Property')
-    record_id     VARCHAR2(10),         -- ID del registro afectado (ej. client_id, property_id)
-    action_type   VARCHAR2(10),         -- 'UPDATE', 'INSERT', 'DELETE'
-    change_date   DATE DEFAULT SYSDATE, -- Fecha del cambio
-    column_name   VARCHAR2(50),         -- Nombre del atributo modificado
-    old_value     VARCHAR2(500),        
-    new_value     VARCHAR2(500)         
+create table additional_rates ( -- Tabla que incluye la informacion de tarifas adicionales como impuestos o cosas como la administración de las casas
+   rate_name   varchar2(50) primary key,
+   rate_value  number(7,2) not null check ( rate_value >= 0 ),
+   unit        varchar2(10) default '%' not null check ( unit in ( '%' ) ),
+   rate_type   varchar2(20) default '-' not null check ( rate_type in ( 'TAX',
+                                                                      'COMMISSION' ) ),
+   description varchar2(100) default '-' not null
 );
 
+
+create table control_changes (
+   change_id   number
+      generated always as identity
+   primary key,
+   table_name  varchar2(50),         -- Nombre de la tabla modificada (ej. 'Client', 'Property')
+   record_id   varchar2(10),         -- ID del registro afectado (ej. client_id, property_id)
+   action_type varchar2(10),         -- 'UPDATE', 'INSERT', 'DELETE'
+   change_date date default sysdate, -- Fecha del cambio
+   column_name varchar2(50),         -- Nombre del atributo modificado
+   old_value   varchar2(500),
+   new_value   varchar2(500)
+);
+
+create table notification (
+   hour_date date default trunc(sysdate) not null,
+   message   varchar2(500) default '-' not null,
+   typology  varchar2(100) default '-' not null check ( typology in ( 'WAITING LIST',
+                                                                     'FIRMED CONTRACT',
+                                                                     'RENT SOLICITUDE',
+                                                                     'SUCCESSFULL PAYMENT',
+                                                                     'UPDATED PROPERTY' ) ),
+   username  varchar2(20) default '-' not null,
+   foreign key ( username )
+      references account_information ( username )
+         on delete cascade
+)
